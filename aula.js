a7ba06cc0de0dba8dd1027303ad74be9789f54f8
Gatojo <gatojogu1@gmail.com>
2022-09-08 13:02:45 -0300

    curso de javascript

---
+++ aula.js
@@ -0,0 +1,46 @@
+//  AULA 1
+// let idade = 5;
+// console.log(idade);
+// let altura = 174;
+// console.log(altura);
+// let campeao ='lulu';
+// console.log(campeao);
+// let rota = "suporte";
+// console.log(rota);
+// let cor = "roxo";
+// console.log(cor);
+
+// AULA 2
+// const valorIngressoAdulto = 20;
+// console.log(valorIngressoAdulto);
+
+// AULA 3
+// let nome ="Gustavo"; //string literal
+// let idade ="22/10/2222"; // number literal
+// let estaAprovado = true; //boolean
+// let sobrenome = undefined; //undefined
+// let corSelecionado =null; //Redefinir um valor
+// let corFavorita ="preto";
+// let jogoFavorito ="league of legends";
+// let time ="chafu";
+
+// let pessoa = {
+//     nome:"Gustavo",
+//     idade:"22/10/2222",
+//     estaAprovado: true,
+//     sobrenome:"Domingos",
+//     corFavorita:"preto",
+//     jogoFavorito:"league of legends",
+//     time:"chafu",
+// };
+
+// console.log(pessoa);
+
+// AULA 4 arrays
+
+// let familia = [26,45,50,17];
+// console.log(familia.length);
+// console.log(familia[2]);
+
+// let nomeDoColega = ["Brasil","São Paulo","Jonnatham Frank",23,"Age of Empires"];
+// console.log("O nome do colega é:"+ nomeDoColega[2],"Ele nasceu em "+  nomeDoColega[0],nomeDoColega[1],"Ele tem:" + nomeDoColega[3],"Anos " + "Ele sempre perde no " + nomeDoColega[4]);
